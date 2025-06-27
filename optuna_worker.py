bc5f9b0ff724ae9c095d2af8be78edd9dc855599
rexxa <rexxa.som@kakaoenterprise.com>
2025-06-28 01:43:20 +0900

    Update optuna_worker.py

--- optuna_worker.py
+++ optuna_worker.py
@@ -91,4 +91,4 @@ if __name__ == "__main__":
     simple_pb = SimpleTqdmCallback(total_trials=1)
     study.optimize(objective,
                    n_trials=total_trials,
-                   callbacks=[tqdm_callback])
+                   callbacks=[simple_pb])
